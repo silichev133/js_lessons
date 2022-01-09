232a4fa62394e653c54bd870ff18881d73950cd5
Ilya Silichev <silich133@gmail.com>
2022-01-09 19:50:37 +0300

    This is first commit for this repo

---
+++ easycode/first_block/app.js
@@ -0,0 +1,18 @@
+console.log("-------Start-------");
+
+let users = [
+  {
+    name: "Silich",
+    age: 30,
+  },
+  {
+    name: "Chilis",
+    age: 40,
+  },
+];
+
+for (let value of users) {
+  console.log(value.age);
+}
+
+console.log("-------end---------");
---
+++ petrechenko/script.js
@@ -0,0 +1,6 @@
+const answers = [];
+
+answers[0] = prompt("What is your name?");
+answers[1] = prompt("How old you?");
+
+document.write(answers);
