c44693e881652c0af5c5150cc823fd6c00b1b3f4
Ilya Silichev <silich133@gmail.com>
2022-01-10 20:56:05 +0300

    Lesson about arguments

--- easycode/first_block/app.js
+++ easycode/first_block/app.js
@@ -1,18 +1,8 @@
 console.log("-------Start-------");
 
-let users = [
-  {
-    name: "Silich",
-    age: 30,
-  },
-  {
-    name: "Chilis",
-    age: 40,
-  },
-];
-
-for (let value of users) {
-  console.log(value.age);
+function foo() {
+  console.log(arguments);
 }
 
+foo();
 console.log("-------end---------");
