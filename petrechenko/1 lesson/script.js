71d28a0bbde22a14132e62980dd09c9d39122fa3
Ilya Silichev <silich133@gmail.com>
2022-01-16 09:08:10 +0300

    first practice 2

--- petrechenko/1 lesson/script.js
+++ petrechenko/1 lesson/script.js
@@ -7,3 +7,13 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
+
+const a = prompt("Один из последних просмотренных фильмов: ");
+const b = prompt("На сколько вы его оцените: ");
+const c = prompt("Один из последних просмотренных фильмов: ");
+const d = prompt("На сколько вы его оцените: ");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
